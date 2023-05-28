85134493d16ed45821b13af776fcc4ec257b1fce
Wellington Saturnino <wellingtonsaturnino30@hotmail.com>
2023-05-28 11:27:59 -0300

    A project of the blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description":" Marvel studios anuncia novo filme do homem de ferro.",
+    "photoCover":"https://conteudo.imguol.com.br/c/entretenimento/96/2020/08/07/iron-man-1596813808466_v2_900x506.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Novo fime anunciado.",
+    "description": "bla bla bla bla ",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2021/12/9955266_x720.png"
+  },
+  {
+    "id": "3",
+    "title": "Novo filme do Pantera Negra.",
+    "description": "bla bla bla bla ",
+    "photoCover":"https://p2.trrsf.com/image/fget/cf/1200/675/middle/images.terra.com/2022/10/04/1001119913-saiba-quem-e-a-nova-pantera-negra.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Novo fime dos Xmen.",
+    "description": "bla bla bla bla ",
+    "photoCover":"https://i.ytimg.com/vi/tbQn8tJw_so/maxresdefault.jpg"
+  },
+]
